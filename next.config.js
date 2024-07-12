4c753d18faadb9536de274598e2f63e46f80ba37
gcloudan <85101154+gcloudan@users.noreply.github.com>
2024-07-12 07:08:27 +0000

    d

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-module.exports = withBundleAnalyzer({})
+module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+}
